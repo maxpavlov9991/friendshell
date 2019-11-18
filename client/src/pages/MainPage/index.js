a9654f1ef80f0783aea89e256520a7497ac69ca9
Max <maxpavlov9991@gmail.com>
2019-11-19 02:29:39 +0300

    fixed project structure

---
+++ client/src/pages/MainPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css'
+
+
+function LoginPage() {
+  return (
+    <div className='page-main'>
+        <h1>Main PAGE!!!</h1>
+    </div>
+  );
+}
+
+export default LoginPage;
